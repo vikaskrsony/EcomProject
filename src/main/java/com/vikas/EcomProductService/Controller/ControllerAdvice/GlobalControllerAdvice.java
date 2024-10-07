e10b47f4f6c6567e084e174201915f0333024717
vikas <vikassony359@gmail.com>
2024-10-08 00:31:21 +0530

    added some more code

--- src/main/java/com/vikas/EcomProductService/Controller/ControllerAdvice/GlobalControllerAdvice.java
+++ src/main/java/com/vikas/EcomProductService/Controller/ControllerAdvice/GlobalControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.vikas.EcomProductService.Controller.ControllerAdvice;
 
 import com.sun.net.httpserver.HttpsServer;
+import com.vikas.EcomProductService.DTO.ErrorResponseDTO;
+import com.vikas.EcomProductService.Exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,15 +10,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
-    @ExceptionHandler(value = NullPointerException.class)
-    public ResponseEntity<String> handleNullPointerException(Exception ex){
-        String exceptionResponse =
-                "error" + ex.getMessage() + ", code :  " + HttpStatus.INTERNAL_SERVER_ERROR;
-        return ResponseEntity.ok(exceptionResponse);
+    @ExceptionHandler(value = ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleProductNotFoundException(Exception ex) {
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
+        errorResponseDTO.setMessage(ex.getMessage());
+        errorResponseDTO.setMessageCode(404);
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = ArithmeticException.class)
-    public ResponseEntity<String> handleArithmeticException(Exception ex){
+    public ResponseEntity<String> handleArithmeticException(Exception ex) {
         String exceptionResponse =
                 "ye wala error hai bhai :-> " + ex.getMessage() + ", code :  " + HttpStatus.INTERNAL_SERVER_ERROR;
         return ResponseEntity.ok(exceptionResponse);
