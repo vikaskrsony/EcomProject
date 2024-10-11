5362a1923adc0c6622763949e4093961beadd93c
vikas <vikassony359@gmail.com>
2024-10-11 10:09:28 +0530

    added some more code

--- src/main/java/com/vikas/EcomProductService/Model/BaseModel.java
+++ src/main/java/com/vikas/EcomProductService/Model/BaseModel.java
@@ -1,9 +1,6 @@
 package com.vikas.EcomProductService.Model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
