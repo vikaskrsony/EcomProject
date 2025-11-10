066f05ac913e473fa9ee752613695c6cd890c7dc
vikas <vikassony359@gmail.com>
2025-11-10 15:55:01 +0530

    added some changes

--- src/main/java/com/ecom/ProductService/controllers/ProductController.java
+++ src/main/java/com/ecom/ProductService/controllers/ProductController.java
@@ -1,14 +1,10 @@
 package com.ecom.ProductService.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-//    public  getSingleProduct(Long id){
-//
-//    }
 }
